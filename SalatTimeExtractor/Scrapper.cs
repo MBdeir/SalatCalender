07edd9fd76ea88f9b0142bbfed1dbc7ecaf818e9
MBdeir <moey.bdeir@gmail.com>
2025-06-28 17:08:16 +1000

    code cleanup

--- SalatTimeExtractor/Scrapper.cs
+++ SalatTimeExtractor/Scrapper.cs
@@ -2,10 +2,8 @@
 
 public static partial class Scrapper
 {
-
     public static async Task<SalatDTO> Init(City city)
     {
-        //return await Sydney.Run();
         return city switch
         {
             City.Sydney => await Sydney.Run(),
